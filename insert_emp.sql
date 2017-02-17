e4b0e6b3e66527690a72bceb65dbb91934ad368c
himanshurawlani <himanshurawlani@hotmail.com>
2017-02-18 01:57:59 +0530

    Understanding syntax

---
+++ insert_emp.sql
@@ -0,0 +1,11 @@
+insert into employee values
+(seq_emp_id.nextval,'Himanshu','Rawlani','Manager',2000,'M');
+
+insert into employee values
+(seq_emp_id.nextval,'Jayesh','Saita','Analyst',1500,'M');
+
+insert into employee values
+(seq_emp_id.nextval,'Vignesh','Zambre','Support',1100,'M');
+
+insert into employee values
+(seq_emp_id.nextval,'Mihir','Joshi','Support',1100,'M');
