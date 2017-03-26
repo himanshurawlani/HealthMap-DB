47e3f3adc79c7cf715358ef33c67e834aa956833
himanshurawlani <himanshurawlani@hotmail.com>
2017-03-26 20:40:34 +0530

    Horizontal fragmentation completed (insert/update/delete)

--- create_table_alerts_maharashtra_db.sql
+++ create_table_alerts_maharashtra_db.sql
@@ -6,20 +6,3 @@ message varchar2(35) NOT NULL
 
 ALTER TABLE alert_maharashtra
 ADD CONSTRAINT pk_alert_maharashtra PRIMARY KEY (message_id);
-
-/* Creating a sequence so as to AUTO INCREMENT the primary key field */
-create sequence alert_maharashtra_id
-start with 1
-maxvalue 9999
-nocycle;
-
-/* Setting up trigger to be called on every insertion */
-CREATE OR REPLACE TRIGGER alert_maharashtra_trigger
-before insert on alert_maharashtra
-for each row
-begin 
-	Select alert_maharashtra_id.nextval
-	into :new.message_id
-	from dual;
-end;
-/
--- links_server_to_mah_and_guj.sql
+++ links_server_to_mah_and_guj.sql
@@ -8,25 +8,30 @@ connect to gujrat identified by gujrat using 'Oracle_1';
 
 /* Trigger to distribute the data insered in Healthmap_DB to respective DDB */
 CREATE OR REPLACE TRIGGER distributer
-AFTER INSERT ON alerts
+AFTER INSERT OR UPDATE ON alerts
 FOR EACH ROW
 BEGIN
 	if :new.state_name='gujrat' then
-		insert into gujrat.alert_gujrat@link_to_gujrat
+		insert into alert_gujrat@link_to_gujrat
 		values (:new.message_id,:new.priority,:new.message);
 	else
-		insert into maharashtra.alert_maharashtra@link_to_maharashtra
+		insert into alert_maharashtra@link_to_maharashtra
 		values (:new.message_id,:new.priority,:new.message);
 	end if;
 END;
 /
 
-/* Errors
-
-SQL> select * from alert_gujrat@link_to_gujrat;
-select * from alert_gujrat@link_to_gujrat
-                           *
-ERROR at line 1:
-ORA-12154: TNS:could not resolve the connect identifier specified
-
-*/
+/* Trigger to maintain data consistency on deletion */
+CREATE OR REPLACE TRIGGER deleter
+BEFORE DELETE ON alerts
+FOR EACH ROW
+BEGIN
+	if :old.state_name='gujrat' then
+		delete from alert_gujrat@link_to_gujrat
+		where message_id=:old.message_id;
+	else
+		delete from alert_maharashtra@link_to_maharashtra
+		where message_id=:old.message_id;
+	end if;
+END;
+/
