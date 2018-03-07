bbc65e46a7ab674c644bdca73512ba7f31c0428f
Himanshu Rawlani <himanshurawlani@hotmail.com>
2018-03-07 21:24:34 +0530

    Delete insert_statements_healthmap_db.sql

--- insert_statements_healthmap_db.sql
+++
@@ -1,9 +0,0 @@
-
-insert into alerts
-values (1,'maharashtra',6,'Air borne disease');
-	
-insert into alerts(state_name,priority,message)
-values (1,'gujrat',6,'Water borne disease');
-
-/* Don't forget to commit */
-commit;
